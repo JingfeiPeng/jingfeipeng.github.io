7801d25eb0feb1b6fccb03cb67be848bcc62e956
Jingfei Peng <36685292+JingfeiPeng@users.noreply.github.com>
2019-05-08 22:39:16 -0400

    Added cover page

---
+++ Navbar (2).php
@@ -0,0 +1,22 @@
+<nav id="head" class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+  <a href="#"><img src="./assets/companylogo.jpg" height="80" width="180" alt="logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item bold">
+        <a class="nav-link" href="#jumbo">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item bold">
+        <a class="nav-link" href="#services">Our Services</a>
+      </li>
+	  <li class="nav-item bold">
+        <a class="nav-link" href="#staff">Our Team</a>
+      </li>
+    </ul>
+    <div id="phone">
+		<i class="fas fa-phone"></i> (647) 700-6838
+    </div>
+	</div>
+</nav>
